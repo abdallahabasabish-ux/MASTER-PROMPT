6cee31a4a42a3fe74f8b31cf301f0159a66e1bc2
Abdallah Abas <abdallahabasabish@gmail.com>
2026-08-11 02:31:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-// Student Pages
+// Student
 import StudentRoute from "./components/StudentRoute";
 import StudentDashboard from "./pages/student/Dashboard";
 import Subscription from "./pages/student/Subscription";
+import LessonsList from "./pages/student/LessonsList";
+import LessonViewer from "./pages/student/LessonViewer";
+
+// Admin
+import AdminRoute from "./components/AdminRoute";
+import AdminPayments from "./pages/admin/Payments";
 
 export default function App() {
   return (
@@ -22,6 +28,11 @@ export default function App() {
           {/* Student Protected */}
           <Route path="/student" element={<StudentRoute><StudentDashboard /></StudentRoute>} />
           <Route path="/student/subscription" element={<StudentRoute><Subscription /></StudentRoute>} />
+          <Route path="/student/lessons" element={<StudentRoute><LessonsList /></StudentRoute>} />
+          <Route path="/student/lessons/:lessonId" element={<StudentRoute><LessonViewer /></StudentRoute>} />
+
+          {/* Admin Protected */}
+          <Route path="/admin/payments" element={<AdminRoute><AdminPayments /></AdminRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
